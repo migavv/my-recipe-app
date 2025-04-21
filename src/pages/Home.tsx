dc7ea7de53dd0ddb60163e4b67edc2f5605209ca
migavv <migaviria@outlook.com>
2025-04-21 17:30:30 -0500

    switch recipe to spanish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,8 @@ export default function Home() {
             <div className="max-w-5xl mx-auto">
                 <header className="text-center mb-10">
                     <h1 className="text-5xl sm:text-6xl font-extrabold text-pink-600 drop-shadow-sm">
-                        🍳 Let's Make Something Yummy!
+                        Preparemos una receta!
                     </h1>
-                    <p className="text-xl text-gray-700 mt-2">Choose a recipe to get started</p>
                 </header>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
