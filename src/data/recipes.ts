8a78cf1a9b94516e237d013888adf44a9dc47f9b
migavv <migaviria@outlook.com>
2025-04-21 21:29:54 -0500

    fix assets route

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -16,37 +16,37 @@ export const recipes: Recipe[] = [
     {
         id: "sw",
         title: "Sánduche",
-        image: "/images/sw/cover.png",
+        image: "/my-recipe-app/images/sw/cover.png",
         steps: [
             {
-                image: "/images/sw/1.png",
+                image: "/my-recipe-app/images/sw/1.png",
                 text: "Coloca dos tajadas de pan sobre la mesa",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             },
             {
-                image: "/images/sw/2.png",
+                image: "/my-recipe-app/images/sw/2.png",
                 text: "Toma una loncha de queso del paquete",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             },
             {
-                image: "/images/sw/3.png",
+                image: "/my-recipe-app/images/sw/3.png",
                 text: "Pon el queso sobre una de las tajadas de pan",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             },
             {
-                image: "/images/sw/4.png",
+                image: "/my-recipe-app/images/sw/4.png",
                 text: "Pon la segunda tajada de pan sobre el queso",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             },
             {
-                image: "/images/sw/5.png",
+                image: "/my-recipe-app/images/sw/5.png",
                 text: "Presiona el sanduche entre tus manos",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             },
             {
-                image: "/images/sw/6.png",
+                image: "/my-recipe-app/images/sw/6.png",
                 text: "Lo lograste!",
-                audio: "/audio/test_audio.mp3"
+                audio: "/my-recipe-app/audio/test_audio.mp3"
             }
         ]
     }
