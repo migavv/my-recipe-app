8cd3838fbf4b881b4fc35e734ea2d1e91537cd9c
migavv <migaviria@outlook.com>
2025-04-21 20:23:32 -0500

    constant card aspect ratio

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 export default function RecipeCard({ recipe }: { recipe: Recipe }) {
     return (
         <Link to={`/recipe/${recipe.id}`}>
-            <div className="bg-white rounded-3xl shadow-lg p-4 hover:scale-105 hover:shadow-xl transition-transform duration-200 cursor-pointer text-center h-72 flex flex-col justify-between">
-                <img
-                    src={recipe.image}
-                    alt={recipe.title}
-                    className="rounded-xl w-full h-55 object-cover mb-3 border-4 border-yellow-200"
-                />
+            <div className="bg-white rounded-3xl shadow-lg p-4 hover:scale-105 hover:shadow-xl transition-transform duration-200 cursor-pointer text-center h-full flex flex-col justify-between">
+                <div className="w-full aspect-square mb-3 overflow-hidden rounded-xl border-4 border-yellow-200">
+                    <img
+                        src={recipe.image}
+                        alt={recipe.title}
+                        className="w-full h-full object-cover"
+                    />
+                </div>
                 <h2 className="text-2xl font-bold text-purple-600">{recipe.title}</h2>
             </div>
         </Link>
